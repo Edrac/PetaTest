011edeccb1e7569897a68e8360a3696c48f3b1bb
Edison Acuna <edisonricardoac@hotmail.com>
2021-09-28 19:08:35 -0500

    Mapped column names with special characters to class properties

--- PetaTest/Models/Pokemon.cs
+++ PetaTest/Models/Pokemon.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CsvHelper.Configuration.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,17 +9,23 @@ namespace PetaTest.Models
 {
     public class Pokemon
     {
+        [Name("#")]
         public int Id { get; set; }
+        [Key]
         public string Name { get; set; }
+        [Name("Type 1")]
         public string Type1 { get; set; }
 #nullable enable
+        [Name("Type 2")]
         public string? Type2 { get; set; }
 #nullable disable
         public int Total { get; set; }
         public int HP { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
+        [Name("Sp. Atk")]
         public int SpAtk { get; set; }
+        [Name("Sp. Def")]
         public int SpDef { get; set; }
         public int Speed { get; set; }
         public int Generation { get; set; }
