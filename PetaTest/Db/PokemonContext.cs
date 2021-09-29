541ece7b4d565191e36565a7f4836b4722000ece
Edison Acuna <edisonricardoac@hotmail.com>
2021-09-28 19:11:35 -0500

    Db context added for Pokemons

---
+++ PetaTest/Db/PokemonContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetaTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetaTest.Db
+{
+    public class PokemonContext : DbContext
+    {
+        public PokemonContext(DbContextOptions<PokemonContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pokemon> PokemonList { get; set; }
+    }
+}
